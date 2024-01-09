5950b33466df3079349007f13ef6e29925505425
Matias Tito <matiastito1998@gmail.com>
2024-01-09 02:39:56 -0300

    commiteo nuevos cambios

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,25 +1,35 @@
+import React, { useState } from "react";
 import LogoSneakers from "@/assets/images/logo.svg";
 import AvatarImage from "@/assets/images/image-avatar.png";
-import MenuIcons from "@/components/icons/MenuIcons";
-import CartIcon from "@/components/icons/CartIcon";
-import CloseIcons from "@/components/icons/CloseIcons";
+import MainIcons from "@/components/icons/MainIcons.jsx";
+import CloseIcons from "@/components/icons/CloseIcons.jsx";
+import CartIcon from "@/components/icons/CartIcon.jsx";
 
 const MainHeader = () => {
-  const handleOpenMenu = () => {};
-  const handleCloseMenu = () => {};
+  const [navClass, setNavClass] = useState("hidden h-full w-2/5 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0");
+
+  const handleOpenMenu = () => {
+    setNavClass(
+      "absolute left-0 top-0 flex h-full w-2/5 flex-col gap-y-5 bg-slate-400 p-10 font-bold md:mr-auto md:flex md:flex-row md:gap-4 md:static md:p-0",
+    );
+  };
+
+  const handleCloseMenu = () => {
+    setNavClass("hidden h-full w-2/5 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0")
+  };
 
   return (
     <header className="container mx-auto flex items-center gap-8 p-6 px-4">
       <button className="md:hidden" onClick={handleOpenMenu}>
-        <MenuIcons />
+        <MainIcons />
       </button>
       <img
         src={LogoSneakers}
         alt="LogoSneakers"
         className="mb-1 mr-auto h-5 md:mr-0"
       />
-      <nav className="absolute left-0 top-0 flex h-full w-2/5 flex-col gap-y-5 bg-slate-400 p-10 font-bold md:mr-auto md:flex md:flex-row md:gap-4">
-        <button className="mb-12" onClick={handleCloseMenu}>
+      <nav className={navClass}>
+        <button className="mb-12 md:hidden" onClick={handleCloseMenu}>
           <CloseIcons />
         </button>
         <a href="#"> Collections</a>
